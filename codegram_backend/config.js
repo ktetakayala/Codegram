875af52e13abd4afe167662f5a9c0383e38c3729
ktetakayala <ktetakayala@gmail.com>
2023-07-14 09:27:46 +0530

    init

---
+++ codegram_backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOBD_URL: "mongodb+srv://vasu_mukku:mohan@vasu-01.grbhl9w.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "fhgsefuilhgvsdfhvldgvigdfbkuvgdyukbyudglvsg",
+};
